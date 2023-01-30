29b91465361da018fc3618d0e11a37093b654a86
critz㌹ <pokemonmasterswapnil@gmail.com>
2023-01-30 22:57:15 +0530

    Prob final commit

--- MethAssignment.py
+++ MethAssignment.py
@@ -50,6 +50,7 @@ def row_to_reduce(x):
         return False
 
 i,j=map(int,input("Enter the dimensions of the matrix: ").strip().split())
+
 zero_row=[0]*(j+1)
 
 if i==0 or j==0:
@@ -69,7 +70,7 @@ else:
                 
         matrix.sort(reverse=True)
             
-        while check_echelon(matrix)==False:                                        #Converts to echelon form
+        while check_echelon(matrix)==False:                                             #Converts to echelon form
             y=row_to_reduce(matrix)
             if y!=0:
                 leading_index=matrix[y].index(1)
@@ -83,16 +84,16 @@ else:
                 matrix[x]=leading_one(matrix[x])
                 
             matrix.sort(reverse=True)
-
-        for row in range(-1,-len(matrix),-1):                                       #Echelon to RREF form
-            for next_row in range(row,0):                                           #Iterates from the given row to last row
+        
+        for row in range(-1,-len(matrix),-1):                                           #Echelon to RREF form
+            for next_row in range(row,0):                                               #Iterates from the given row to last row
                 
                 if matrix[next_row]!=zero_row:
-                    pivot_pos=matrix[next_row].index(1)                             #Getting the pivot positions of the next row 
-                    multiplier=matrix[row-1][pivot_pos]                             #Getting the factor to multiply the row with
+                    pivot_pos=matrix[next_row].index(1)                                 #Getting the pivot positions of the next row 
+                    multiplier=matrix[row-1][pivot_pos]                                 #Getting the factor to multiply the row with
                 
                     if pivot_pos!=0:
-                        for y in range(pivot_pos,j+1):                              #Row subtraction
+                        for y in range(pivot_pos,j+1):                                  #Row subtraction
                             matrix[row-1][y]-=multiplier*matrix[next_row][y]
                     else:
                         continue
@@ -103,7 +104,7 @@ else:
                 x[y]=round(x[y],3)        
             print(x)
 
-        free_var=[]                                                                 #List to store positions of free variables
+        free_var=[]                                                                     #List to store positions of free variables
         
         if len(matrix)>1:
             for x in range(len(matrix)-1):
@@ -117,7 +118,7 @@ else:
             else:                                                                       
                 free_var.extend(range(matrix[x+1].index(1)+2,j+1))                      #Checks if there are still free variables after hitting the last row
 
-        if len(free_var)!=0:                                                        #If free variables exist then...
+        if len(free_var)!=0:                                                            #If free variables exist then...
             
             parametric=[]
             for x in matrix:
@@ -126,16 +127,16 @@ else:
                     vector=[]
                     for y in free_var:
                         vector.append(x[y-1]) 
-                    parametric.append(vector)                                       #Storing the vectors in parametric list
+                    parametric.append(vector)                                           #Storing the vectors in parametric list
 
             to_print="x = "    
             for x in range(len(free_var)):
                 
                 vector=[]
-                for y in parametric:                                                #Taking the corresponding values from the parametric list and appending to vector
+                for y in parametric:                                                    #Taking the corresponding values from the parametric list and appending to vector
                     vector.append(-y[x])
                 
-                for z in free_var:                                                  #Adding zeroes for the other free variables and ones for itself
+                for z in free_var:                                                      #Adding zeroes for the other free variables and ones for itself
                     if free_var[x]!=z:
                         vector.insert(z-1,0)
                 else:
@@ -144,5 +145,5 @@ else:
                 to_print+=str(vector)+"x"+str(free_var[x])+"+"
 
             print("\n"+to_print.rstrip("+"))
-        else:                                                                       #If free varaibles don't exist then...
+        else:                                                                           #If free varaibles don't exist then...
             print("\nNo free variables thus, only trivial solutions exist")       
